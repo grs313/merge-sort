7a414c5bfea85e6a07e57f094982c80a8acad5c3
grs313 <sipos.david996@gmail.com>
2023-06-05 19:52:06 +0200

    add isSorted method boolean

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
         System.out.println("Merge sort");
     }
     public static boolean isSorted(int[] array){
+        for (int i = 0, i < array.length - 1; i++){
+            if(array[i] > array[i + 1]){
+                return false;
+            }
+        }
         return false;
     }
     public static int[] merge(int[] left, int[] right){
